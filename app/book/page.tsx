f99712af4b76547de8d552238c059ab5e6e214a7
Emranbd31 <emranbd31@gmail.com>
2025-10-22 10:45:59 +0100

    Delete all content files - keep only configuration

--- app/book/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamicImport from 'next/dynamic';
-
-export const dynamic = 'force-dynamic';
-
-const BookClient = dynamicImport(() => import('./BookClient'), { ssr: false });
-
-export default function Page() {
-  return <BookClient />;
-}
