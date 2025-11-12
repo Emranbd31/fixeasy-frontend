1cbb884446e20278473af8aea1a1f04fdbca0e65
Emran Ahmed Begum <emranbd31@gmail.com>
2025-11-12 19:53:13 +0000

    chore(dev): ignore Windows system files in watcher to suppress Watchpack warnings

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  // Prevent Watchpack from probing Windows system files (pagefile/hiberfil/swapfile)
+  // which can produce EINVAL lstat errors on some systems. Quiet these in dev.
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = Object.assign({}, config.watchOptions, {
+        ignored: [
+          '**/node_modules/**',
+          'C:\\pagefile.sys',
+          'C:\\hiberfil.sys',
+          'C:\\swapfile.sys'
+        ],
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
